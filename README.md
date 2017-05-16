018b776f85dd9f0a5e11d133878f83629f2f0ba1
Douglas Silva <douglas_hss@hotmail.com.br>
2017-05-15 21:50:52 -0300

    - Writing the step 1b of Porter's stemming algorithm.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 This repository will keep all algorithms which is being developed for Advanced Topics in Artificial Intelligence module
 
 Stemming Algorithms:
-1. [The Porter Stemming Algorithm!](http://snowball.tartarus.org/algorithms/porter/stemmer.html)
+1. [The Porter Stemming Algorithm](http://snowball.tartarus.org/algorithms/porter/stemmer.html)
--- porter_stemmer.py
+++ porter_stemmer.py
@@ -3,12 +3,60 @@
 
 class PorterStemmer(object):
 
+    VOWELS = "aeiou"
+
     # ###################
     # # PRIVATE METHODS #
     # ###################
+    def _has_vowels(self, word):
+        """Method which checks whether a word has vowels or not.
+
+            @param word: word :str: that is being stemmed.
+
+            @return: :boolean:
+        """
+        for vowel in self.VOWELS:
+            if vowel in word:
+                return True
+        return False
+
+    def _is_vowel(self, word, char_index):
+        """Method which identifies whether a character is a vowel or not.
+
+            @param word: word :str: that is being stemmed.
+            @param char_index: letter position :int: in the word.
+
+            @return: :boolean:
+        """
+        if word[char_index] in self.VOWELS:
+            return True
+
+        elif word[char_index] == "y" and char_index > 0:
+            return not self._is_vowel(word, char_index - 1)
+
+        else:
+            return False
+
+    def _measures_word(self, word):
+        """Method which count the number of VC in a word.
+            A C represents a sequence of consonants(c), while a V denotes a sequence of vowels(v).
+
+            @param word: word :str: that is being stemmed.
+
+            @return: quantity of "VC" in inputted word.
+        """
+        vc_chain = ""
+        for char_index, _ in enumerate(word):
+            if self._is_vowel(word, char_index):
+                vc_chain += "v"
+            else:
+                vc_chain += "c"
+
+        return vc_chain.count("vc")
 
     def _step_1a(self, word):
-        """Method which executes step 1a of Porter stemming algorithm.
+        """Method which executes step 1a of the Porter stemming algorithm.
+
             @param word: word :str: that should be stemmed.
 
             @return: word stem :str:
@@ -19,9 +67,67 @@ class PorterStemmer(object):
                    ("s", "")]
 
         for sufix, replacement in sufixes:
-            if sufix in word:
+            if word.endswith(sufix):
                 return word.replace(sufix, replacement)
         return word
+
+    def _step_1b(self, word):
+        """Method which executes step 1b of the Porter stemming algorithm.
+
+            @param word: word :str: that is being stemmed.
+
+            @return: word stem :str:
+        """
+
+        def _ends_with_double_consonants(stem):
+            """Function which checks if a stem ends with double consonants.
+
+                @param stem: stem :str: to be checked.
+
+                @return: :boolean:
+             """
+            return (len(stem) > 1 and
+                    not self._is_vowel(stem, -1)
+                    and stem[-1] == stem[-2])
+
+        def _ends_with_cvc(stem):
+            """Function which checks if a stem ends with a consonant, a vowel and a consonant
+                respectively.
+
+                @param stem: stem :str: to be checked.
+
+                @return: :boolean:
+             """
+            return (len(stem) > 2 and
+                    not self._is_vowel(stem, -3) and
+                    self._is_vowel(stem, -2) and
+                    not self._is_vowel(stem, -1))
+
+        if word.endswith("eed"):
+            stem = word.replace("eed", "ee")
+            return stem if self._measures_word(stem) > 0 else word
+
+        apply_complementary_rules = False
+        for sufix in ["ed", "ing"]:
+            if word.endswith(sufix):
+                stem = word.replace(sufix, "")
+                if self._has_vowels(stem):
+                    apply_complementary_rules = True
+
+        if not apply_complementary_rules:
+            return word
+
+        for sufix in ["at", "bl", "iz"]:
+            if stem.endswith(sufix):
+                return stem + "e"
+
+        if stem[-1] not in "lsz" and _ends_with_double_consonants(stem):
+            return stem[:-1]
+
+        if self._measures_word(stem) == 1 and _ends_with_cvc(stem) and stem[-3] not in "wxy":
+            return stem + "e"
+
+        return stem
 
     # ##################
     # # PUBLIC METHODS #
@@ -29,6 +135,7 @@ class PorterStemmer(object):
 
     def stem_word(self, word):
         """Method which executes the stemming process in a word.
+
             @param word: word :str: that should be stemmed.
 
             @return: word stem :str:
@@ -36,5 +143,6 @@ class PorterStemmer(object):
         word = word.lower()  # enforces lowercase
 
         stem = self._step_1a(word)
+        stem = self._step_1b(stem)
 
         return stem
